5ac57e3c48df215cbd438ff4eba022597eb79655
Zulèma van Gom <zulema@mannenmedia.nl>
2016-10-05 11:23:04 +0200

    'Choices'

--- waiter.rb
+++ waiter.rb
@@ -1,5 +1,24 @@
 class Waiter
   def greet_guest
-  print "Buongiorno! Benvenuti nel nostro ristorante! "
+    print "Buongiorno! Benvenuti nel nostro ristorante! "
+  end
+
+  def serve_guest
+    p "Gezellig dat jullie ons restaurant hebben gekozen! Hoe kan ik jullie van dienst zijn?"
+    p "1. Willen jullie de kaart om een pizza te bestellen?"
+    p "2. Wordt het toch iets anders vandaag?"
+    take_order(gets.chomp.to_i)
+  end
+
+  def take_order(order_number)
+
+case(order_number)
+    when 1
+      print "Top! Ik ga meteen de kaart voor jullie halen!"
+    when 2
+      print "Peccato! Hopelijk tot een volgende keer. Buongiorno!"
+    else
+      print "Scuse! Ik heb niet helemaal begrepen denk ik?"
+  end
  end
-end
+ end
